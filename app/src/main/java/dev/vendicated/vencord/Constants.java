016ff97745a1e015b9753f6a9edb6b09dc3718cc
GhostDev15 <christoper.23.2008@gmail.com>
2025-06-15 09:51:15 +0700

    Update Constants.java

--- app/src/main/java/dev/vendicated/vencord/Constants.java
+++ app/src/main/java/dev/vendicated/vencord/Constants.java
@@ -1,5 +1,5 @@
 package dev.vendicated.vencord;
 
 public class Constants {
-    public static final String JS_BUNDLE_URL = "https://github.com/Vendicated/Vencord/releases/download/devbuild/browser.js";
+    public static final String JS_BUNDLE_URL = "https://github.com/kauht/venkord/releases/download/1/browser.js";
 }
